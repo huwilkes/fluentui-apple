34b03d6aa0a160d32f5f357234bf718b1dacca34
huwilkes <huwilkes@microsoft.com>
2023-03-24 16:44:39 -0700

    store branch name

--- scripts/create_localization_pr.sh
+++ scripts/create_localization_pr.sh
@@ -2,8 +2,8 @@
 
 #simple script used to create PRs for the automated localization process
 
-timestamp=$(<$1)
+newBranch="huwilkes/PRActions`date +%s"
 
-git checkout -b 'huwilkes/PRActions$timestamp'
-git push --set-upstream origin huwilkes/PRActions$timestamp
-gh pr create -B huwilkes/PRActions -H huwilkes/PRActions$timestamp --title 'test PR actions w/ script' --body  'new script, nice' -l 'Automated PR'
+git checkout -b $newBranch
+git push --set-upstream origin $newBranch
+gh pr create -B huwilkes/PRActions -H $newBranch --title 'test PR actions w/ script' --body  'new script, nice' -l 'Automated PR'
