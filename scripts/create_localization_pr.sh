62474c34267260419ed80847d02fa06b3c6cfc4d
huwilkes <huwilkes@microsoft.com>
2023-03-28 16:04:10 -0700

    try passing in creds

--- scripts/create_localization_pr.sh
+++ scripts/create_localization_pr.sh
@@ -2,13 +2,15 @@
 
 #simple script used to create PRs for the automated localization process
 
+email=$(<$1)
+name=$(<$2)
 newBranch="huwilkes/PRActions`date +%s`"
 
 date +%s > timestamp
 
 if [[ `git status --porcelain` ]]; then
-  git config --global user.email ${{ secrets.EMAIL }}
-  git config --global user.name ${{ secrets.NAME }}
+  git config --global user.email $email
+  git config --global user.name $name
   git add .
   git commit -m "test commit"
   git checkout -b $newBranch
